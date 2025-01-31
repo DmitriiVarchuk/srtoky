a20700a5e5751cc97c3ab9d0f3b153f7f7063770
Dmytro Varchuk <loltop792@gmail.com>
2025-01-31 15:34:04 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <cctype>
+using namespace std;
+
+int check(const char* str, const char* pattern) {
+    int counter = 0;
+    for (int i = 0; str[i] != '\0'; i++) {
+        for (int j = 0; pattern[j] != '\0'; j++) {
+            if (str[i] == pattern[j]) {
+                counter++;
+            }
+        }
+    }
+    return counter;
+}
+
+int main() {
+
+    char str[] = "";
+    const char digits_pattern[] = "0123456789";
+    const char letters_pattern[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const char alpha_pattern[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+[]{}|;':\",.<>?/`~";
+    cout << "digits: " << check(str, digits_pattern) << endl;
+    cout << "letters: " << check(str, letters_pattern) << endl;
+    cout << "alpha:" << check(str, alpha_pattern) << endl;
+
+
+    return 0;
+}
