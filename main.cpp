57d5f63d839a9dcf05d1033bbaca06510fc8f0c5
Dmytro Varchuk <loltop792@gmail.com>
2025-01-31 15:32:35 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    char str[] = "One Two and You";
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] == ' ') {
+            str[i] = '\t';
+        }
+    }
+    cout << str << endl;
+    return 0;
+}
