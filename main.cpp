7f82822b31a570376967b31014760f1358b29145
Dmytro Varchuk <loltop792@gmail.com>
2025-01-31 15:34:48 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring> 
+using namespace std;
+
+int main() {
+    char str[] = "One Two and you";
+    char* word = strtok(str, " ");
+    int wordCount = 0;
+    while (word != nullptr) {
+        wordCount++;
+        word = strtok(nullptr, " ");
+    }
+    cout << "Number of words: " << wordCount << endl;
+
+    return 0;
+}
